cc51c7a67cd13ab567ec327c34a5734934ec1325
Adam Yiu <adam_yiu@outlook.com>
2023-04-07 15:42:12 -0400

    Create navbar

--- mortgage_calculator/src/App.js
+++ mortgage_calculator/src/App.js
@@ -1,8 +1,20 @@
-import logo from "./logo.svg";
+import React, { useState } from "react";
+import { Grid } from "@mui/material";
+import { Container } from "@mui/system";
 import "./App.css";
+import Navbar from "./Components/Navbar"
 
 function App() {
-  return <div className="App">Hello World!</div>;
+  return <div className="App">
+    <Navbar></Navbar>
+    <Container maxWidth="xl" sx={{marginTop:4}}>
+      <Gridcontainer spacing={5} alignItems="center">
+        <GridItem ></GridItem>
+      </Gridcontainer>
+
+    </Container>
+
+  </div>;
 }
 
 export default App;
